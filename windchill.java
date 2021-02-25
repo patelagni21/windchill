4f43455434c0fdabaf606c3e0468a4dec99961c5
Agni Patel <77357349+patelagni21@users.noreply.github.com>
2021-02-25 10:10:46 -0500

    Add files via upload

---
+++ windchill.java
@@ -0,0 +1,13 @@
+public class WindChill
+{
+   public static void main(String[] args)
+   {
+       double T = Double.parseDouble(args[0]);
+       double v = Double.parseDouble(args[1]);
+      
+       double w = 35.74 + 0.6215 * T + (0.4275 * T - 35.75) * Math.pow(v, 0.16);
+      
+       System.out.println(w);
+
+   }
+}
